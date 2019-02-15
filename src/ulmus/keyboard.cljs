35d07fb23c7f376e0e725c1bca1856f42d21434a
Jeremy Kross <me@jeremykross.com>
2019-02-15 15:32:41 -0500

    flesh out keybaord events.

--- src/ulmus/keyboard.cljs
+++ src/ulmus/keyboard.cljs
@@ -2,5 +2,18 @@
   (:require ulmus.dom
             [ulmus.signal :as ulmus]))
 
-(def up-events-$
+(def up-$
   (ulmus.dom/listen! "keyup" js/document))
+
+(def down-$
+  (ulmus.dom/listen! "keydown" js/document))
+
+(def press-$
+  (ulmus.dom/listen! "keypress" js/document))
+
+(defn press
+  [keycode]
+  (ulmus/filter 
+    (fn [e] (= (.-keyCode e) keycode))
+    press-$))
+
